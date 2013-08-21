e5e427f52bcfedc819d256d07542a672422a4ba2
JollyDuck <jolly_duck@hotmail.co.uk>
2013-08-21 22:10:10 +0100

    Netbeans Commit

---
+++ MinionManage/src/minionmanage/MinionManage.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minionmanage;
+
+/**
+ *
+ * @author James
+ */
+public class MinionManage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hi");
+    }
+}

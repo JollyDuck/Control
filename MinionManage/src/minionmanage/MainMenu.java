4c4e8dc2945907184a1a7819207b290f63ded147
JollyDuck <jolly_duck@hotmail.co.uk>
2013-08-25 18:07:09 +0100

    Begun work on states

---
+++ MinionManage/src/minionmanage/MainMenu.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minionmanage;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ *
+ * @author dad
+ */
+public class MainMenu extends StateBasedGame {
+
+    public MainMenu(String name) {
+        super(name);
+    }
+
+    @Override
+    public void initStatesList(GameContainer gc) throws SlickException {
+        //addState()
+    }
+}

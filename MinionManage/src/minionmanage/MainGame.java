d3ac3a3ecb7feaa2357cd7a20b5c66d6217bcb9f
JollyDuck <jolly_duck@hotmail.co.uk>
2013-08-25 21:17:10 +0100

    Finished setting up states

--- MinionManage/src/minionmanage/MainGame.java
+++ MinionManage/src/minionmanage/MainGame.java
@@ -5,44 +5,50 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
 
-public class MainGame extends BasicGame {
+public class MainGame extends BasicGameState {
 
     MinionManager minionManager;
     WorkManager workManager; //can work manager be integrated into minionManager?
+    int stateID = 0;
 
-    public MainGame(String gamename) {
-        super(gamename);
+    @Override
+    public int getID() {
+        return stateID;
+    }
+
+    public MainGame(int it) {
+        stateID = it;
     }
 
     @Override
-    public void init(GameContainer gc) throws SlickException {
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
         Minion[] setup = {new Minion(), new Minion(), new Minion()};
         minionManager = new MinionManager(setup);
         workManager = new WorkManager();
     }
 
     @Override
-    public void update(GameContainer gc, int i) throws SlickException {
-        Input input = gc.getInput();
-        if (input.isKeyPressed(Input.KEY_1)) {
-            minionManager.assignWork(1, workManager);
-        }
-        else if (input.isKeyPressed(Input.KEY_2)){
-            minionManager.assignWork(2, workManager);
-        }
-
-        minionManager.updateMinions(i, workManager);
-    }
-
-    @Override
-    public void render(GameContainer gc, Graphics g) throws SlickException {
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
         g.drawRect(50, 50, 50, 50);
         g.drawRect(150, 50, 50, 50);
         g.drawRect(250, 50, 50, 50);
         minionManager.drawMinions(g);
         codingDraw(g);
+    }
 
+    @Override
+    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
+        Input input = gc.getInput();
+        if (input.isKeyPressed(Input.KEY_1)) {
+            minionManager.assignWork(1, workManager);
+        } else if (input.isKeyPressed(Input.KEY_2)) {
+            minionManager.assignWork(2, workManager);
+        }
+
+        minionManager.updateMinions(i, workManager);
     }
 
     public void codingDraw(Graphics g) {
--- MinionManage/src/minionmanage/StartUp.java
+++ MinionManage/src/minionmanage/StartUp.java
@@ -7,28 +7,37 @@ package minionmanage;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
 /**
  *
  * @author James
  */
-public class StartUp {
+public class StartUp extends StateBasedGame {
+    
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
-    	public static void main(String[] args)
-	{
-		try
-		{
-			AppGameContainer appgc;
-			appgc = new AppGameContainer(new MainGame("Simple Slick Game"));
-			appgc.setDisplayMode(WIDTH, HEIGHT, false);
-                        appgc.setVSync(true);
-			appgc.start();
-		}
-		catch (SlickException ex)
-		{
-			Logger.getLogger(MainGame.class.getName()).log(Level.SEVERE, null, ex);
-		}
-	}
+    
+    public StartUp(String s) {
+        super(s);
+    }
+    
+    public static void main(String[] args) {
+        try {
+            AppGameContainer appgc;
+            appgc = new AppGameContainer(new StartUp("Simple Slick Game"));
+            appgc.setDisplayMode(WIDTH, HEIGHT, false);
+            appgc.setVSync(true);
+            appgc.start();
+        } catch (SlickException ex) {
+            Logger.getLogger(MainGame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @Override
+    public void initStatesList(GameContainer gc) throws SlickException {
+        this.addState(new MainGame(1));
+    }
 }
